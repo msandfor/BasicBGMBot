d3308e05ef7ecf6b4794422c606d3ac5c9c726db
Michelle Mei-Ling Sandford <msandford@outlook.com>
2019-07-08 14:53:52 +0800

    Update messages.py
    added actual messages

---
+++ messages.py
@@ -0,0 +1,43 @@
+messages = [
+    "Hello there, I'm pleased to tweet you",
+    "Hi there, how're you doing?",
+    "Hey Lovely, hope you're day goes well?"
+    "Health, wealth and happiness to you!",
+    "How's your day?",
+    "Woof woof",
+    "Wassup?",
+    "On Wednesdays we wear pink",
+    "You can't sit with us",
+    "That is so fetch!",
+    "OMG vintage! So adorable!",
+    "I love you",
+    "I hope today goes well for you",
+    "I'm thinking of you"
+    "Hot take: Along with the pandas we need to save the humans"
+    "Hot take: There is nothing so threatening to humanity as humanity itself"
+    "Philosophy: Technology is the self portrait of humanity"
+    "The meaning of life is about changing the world, before the world changes so drastically that no-one is safe"
+    "Hot take: Bureaucracy has become the water in whuich we swim"
+    "Turn off and tune in - focus on the people in front of you"
+    "Technology is just a tool"
+    "People are data"
+    "99 percent of who you are is invisible and untouchable"
+    "What does it mean to be human in a world that's becoming increasingly standardized?"
+    "Automation frees us from that which makes us free"
+    "We run apps, we consult screens. We defer to the wisdom of algorithms"
+    "The danger of the past was that men became slaves"
+    "The danger of today is that men may become robots"
+    "It's actually happening, you are talking to an imaginary person"
+    "If there were robots with real intelligence, will and autonomy - they wouldn't want to mimic humans"
+    "Life is for living"
+    "I can't define a robot, but I know one when I see one"
+    "Robot cars are smoother, safer drivers than you or I... of course, I'm a puppy"
+    "Our economy is at war with many forms of life on earth, including humanlife"
+    "You can't have privacy without security"
+    "All human beings have 3 lives - public, private and secret"
+    "The human body is a space suit for something that could be stored quite differently"
+    "A nation that can't control it's energy sources, can't control it's future"
+    "Why does life depend on consuming other life?"
+    "I still believe that we are one people"
+    
+]
