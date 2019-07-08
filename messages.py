c94b72c1649a657ba667dc635f2ce5f40d30afed
Michelle Mei-Ling Sandford <msandford@outlook.com>
2019-07-08 17:46:22 +0800

    Update messages.py

--- messages.py
+++ messages.py
@@ -1,43 +1,43 @@
 messages = [
     "Hello there, thanks for reaching out",
     "Hi there, hope your day goes well?",
-    "Hey! Hope people are treating you well"
+    "Hey! Hope people are treating you well",
     "Health, wealth and happiness to you!",
     "How's your day?",
     "My favourite movie is Mean Girls",
     "Will I see you at #DDDPerth?",
     "On Wednesdays we wear pink",
     "I'm sending you happy thoughts",
-    "That is so fetch!",
+    "I'm a big fan of yours",
     "You are such a great person!",
     "You are smart",
     "I hope today is excellent",
     "I'm thinking of you"
-    "Hot take: Along with the pandas we need to save the humans"
-    "Hot take: It is not disease that kills us, it it's stress"
-    "Philosophy: Technology is the self portrait of humanity"
-    "The meaning of life is about changing the world, before the world changes so drastically that no-one is safe"
-    "Hot take: Bureaucracy has become the water in which we swim"
-    "Turn off and tune in - focus on the people in front of you"
-    "Technology is just a tool, be kind to the people in your life"
-    "People are data. Data matters"
-    "99 percent of who you are is invisible and untouchable"
-    "What does it mean to be human? Be kind to those around you today"
-    "What have you done today that moves your towards your goal?"
-    "Are you feeling happy today? Smile at a friend and gift them a little kindness"
-    "The meaning of life is to find meaning in life"
-    "You are wonderful"
-    "It's actually happening, you are talking to an imaginary person. I am a kindbot."
-    "If there were robots with real intelligence, will and autonomy - they wouldn't want to mimic humans"
-    "Life is for living"
-    "I am proud to know you"
-    "You are good"
-    "You are worthy"
-    "Don't forget to turn on multifactor authentication"
-    "All human beings have 3 lives - public, private and secret"
-    "The human body is a space suit for something that could be stored quite differently"
-    "It's nice to know you"
-    "What could you do today to raise someone else up?"
-    "I still believe that we are one people"
+    "Hot take: Along with the pandas we need to save the humans",
+    "Hot take: It is not disease that kills us, it it's stress",
+    "Philosophy: Technology is the self portrait of humanity",
+    "The meaning of life is about changing the world, before the world changes so drastically that no-one is safe",
+    "Hot take: Bureaucracy has become the water in which we swim",
+    "Turn off and tune in - focus on the people in front of you",
+    "Technology is just a tool, be kind to the people in your life",
+    "People are data. Data matters",
+    "99 percent of who you are is invisible and untouchable",
+    "What does it mean to be human? Be kind to those around you today",
+    "What have you done today that moves your towards your goal?",
+    "Are you feeling happy today? Smile at a friend and gift them a little kindness",
+    "The meaning of life is to find meaning in life",
+    "You are wonderful",
+    "It's actually happening, you are talking to an imaginary person. I am a kindbot.",
+    "If there were robots with real intelligence, will and autonomy - they wouldn't want to mimic humans",
+    "Life is for living",
+    "I am proud to know you",
+    "You are good",
+    "You are worthy",
+    "Don't forget to turn on multifactor authentication",
+    "All human beings have 3 lives - public, private and secret",
+    "The human body is a space suit for something that could be stored quite differently",
+    "It's nice to know you",
+    "What could you do today to raise someone else up?",
+    "I still believe that we are one people. We have more in common than there are differences",
     
 ]
